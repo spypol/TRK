cf25284813459754e30f28a93b64ffcb7dd344ed
spypol <paulpinier@gmail.com>
2014-02-05 20:42:16 -0500

    Main Page

--- content.php
+++ content.php
@@ -8,23 +8,32 @@
 	<header class="entry-header">
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php trk_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
         
-        <?php
-            /* translators: used between list items, there is a space after the comma */
-            $categories_list = get_the_category_list( __( ', ', 'trk' ) );
-            if ( $categories_list && trk_categorized_blog() ) :
-        ?>
-        <span class="cat-links">
-            <?php printf( __( '%1$s', 'trk' ), $categories_list ); ?>
-        </span>
-        <?php endif; // End if categories ?>
+        <div class="entry-meta">
+            
+            <?php if ( 'post' == get_post_type() ) : ?>
+            
+                <?php trk_posted_on(); ?>
+            
+            <?php endif; ?>
+            
+            <?php
+                /* translators: used between list items, there is a space after the comma */
+                $categories_list = get_the_category_list( __( ' / ', 'trk' ) );
+                if ( $categories_list && trk_categorized_blog() ) :
+            ?>
+            <span class="cat-links">
+                <?php printf( __( '%1$s', 'trk' ), $categories_list ); ?>
+            </span>
+            <?php endif; // End if categories ?>
+            
+            <?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+                <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'trk' ), __( '1 Comment', 'trk' ), __( '% Comments', 'trk' ) ); ?></span>
+            <?php endif; ?>
         
-         <?php echo get_the_post_thumbnail(); ?> 
+        </div><!-- .entry-meta -->
+            
+        <a href="<?php the_permalink(); ?>" rel="bookmark" class="link-img"><?php echo get_the_post_thumbnail(); ?></a>
         
 	</header><!-- .entry-header -->
 
@@ -34,7 +43,7 @@
 	</div><!-- .entry-summary -->
 	<?php else : ?>
 	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'trk' ) ); ?>
+		<?php the_content( __( 'Read more <span class="meta-nav">»</span>', 'trk' ) ); ?>
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', 'trk' ),
@@ -52,14 +61,10 @@
 				if ( $tags_list ) :
 			?>
 			<span class="tags-links">
-				<?php printf( __( 'Tagged %1$s', 'trk' ), $tags_list ); ?>
+				<?php //printf( __( 'Tagged %1$s', 'trk' ), $tags_list ); ?>
 			</span>
 			<?php endif; // End if $tags_list ?>
 		<?php endif; // End if 'post' == get_post_type() ?>
-
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'trk' ), __( '1 Comment', 'trk' ), __( '% Comments', 'trk' ) ); ?></span>
-		<?php endif; ?>
 
 		<?php edit_post_link( __( 'Edit', 'trk' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
